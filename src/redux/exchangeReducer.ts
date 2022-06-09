ebc619afbf36b86f9ccd9ec2af943cf6d143534c
LitvinenkoBD <boris12lit@gmail.com>
2022-06-09 18:59:51 +0300

    added ActualRatesObject type in exchngeReducer.ts

--- src/redux/exchangeReducer.ts
+++ src/redux/exchangeReducer.ts
@@ -1,19 +1,3 @@
-import { currencyAPI } from "../api/api";
-
-enum ExchangeTypes {
-    ADD_BASE_CURRENCY = 'C/SRC/REDUX/EXCHANGE_REDUCER/ADD_BASE_CURRENCY',
-    ADD_ACTUAL_RATES = 'C/SRC/REDUX/EXCHANGE_REDUCER/ADD_ACTUAL_RATES'
-}
-
-type AllRates = 'USD' | 'EUR' | 'RUB'
-
-let initialState = {
-    baseApp: 'USD' as AllRates,
-    interests: ['USD', 'EUR', 'RUB'] as Array<AllRates>,
-    actualRates: [] as Array<Object>,
-    isFetching: false
-}
-
 // actualRates Object
 // base: "USD"
 // date: "2022-06-07"
@@ -24,12 +8,40 @@ let initialState = {
 // rates: {RUB: 60.997338}
 // success: true
 
-const exchangeReducer = (state = initialState, action:any) => {
+import { currencyAPI } from "../api/api";
+
+enum ExchangeTypes {
+    ADD_BASE_CURRENCY = 'C/SRC/REDUX/EXCHANGE_REDUCER/ADD_BASE_CURRENCY',
+    ADD_ACTUAL_RATES = 'C/SRC/REDUX/EXCHANGE_REDUCER/ADD_ACTUAL_RATES'
+}
+
+type AllRates = 'USD' | 'EUR' | 'RUB'
+
+interface ActualRatesObject {
+    base: AllRates;
+    rates: {[key: string]: number};
+    // rates: {[key: AllRates]: number}; 
+    // doesn't work
+    date?: string;
+    motd?: {msg: string, url:string};
+    success?: boolean
+}
+
+let initialState = {
+    baseApp: 'USD' as AllRates,
+    interests: ['USD', 'EUR', 'RUB'] as Array<AllRates>,
+    actualRates: [] as Array<ActualRatesObject>,
+    isFetching: false
+}
+
+export type ExchangeStateType = typeof initialState
+
+const exchangeReducer = (state = initialState, action:ExchangeActionsType):ExchangeStateType => {
     switch (action.type) {
         case ExchangeTypes.ADD_BASE_CURRENCY: {
             return {
                 ...state,
-                baseApp: action.base
+                baseApp: action.base,
             }
         }
         case ExchangeTypes.ADD_ACTUAL_RATES: {
@@ -47,15 +59,31 @@ interface SetBaseActionI {
     type: ExchangeTypes.ADD_BASE_CURRENCY;
     base: AllRates
 }
+interface GetActualRatesActionI {
+    type: ExchangeTypes.ADD_ACTUAL_RATES;
+    actualRates: Array<ActualRatesObject>
+}
+
+
+// Use an index signature to dynamically add properties to an object, 
+// e.g. const obj: {[key: string]: any} = {}. 
+// Index signatures are used when we don't know all of the names of a type's 
+// properties and the type of their values ahead of time.
+
+// rates: {RUB: 60.997338}
+// success: true
+
+
+export type ExchangeActionsType = SetBaseActionI | GetActualRatesActionI
 
 export const setBaseAC = (base:AllRates):SetBaseActionI => ({ type: ExchangeTypes.ADD_BASE_CURRENCY, base });
-export const getActualRatesAC = (actualRates: any) => ({ type: ExchangeTypes.ADD_ACTUAL_RATES, actualRates });
+export const getActualRatesAC = (actualRates: Array<ActualRatesObject>):GetActualRatesActionI => ({ type: ExchangeTypes.ADD_ACTUAL_RATES, actualRates });
 
 
-export const setActualRatesTC = (interests:any, baseApp:AllRates) => {
+export const setActualRatesTC = (interests:AllRates, baseApp:AllRates) => {
     return (dispatch:any) => {
         dispatch(setBaseAC(baseApp))
-        currencyAPI.getArrRates(interests, baseApp).then((response:any) => {
+        currencyAPI.getArrRates(interests, baseApp).then((response:Array<ActualRatesObject>) => {
             dispatch(getActualRatesAC(response))
         })
     }
@@ -65,3 +93,22 @@ export const setActualRatesTC = (interests:any, baseApp:AllRates) => {
 export default exchangeReducer;
 
 
+// Вопрос по Ts
+
+// type AllRates = 'USD' | 'EUR' 
+
+// interface actualRatesObject {
+//     base: AllRates;
+//     rates: {???}
+//     ....    
+//     //rates содержит объект {} с одним свойством которое придет от сервера. Свойство может быть USD | EUR  со значением number
+//     // тот вариант, который я нашел рабочий:
+//      rates: {[key: string]: number};
+//     //но хотелось, чтобы работало по логике, которая строчкой ниже
+//     // rates: {[key: AllRates]: number}; 
+//     // a этот вариант не работает
+// }
+
+// Как реализовать rates: {[key: AllRates]: number} по-другому?
+// Если что Ts предлагает: An index signature parameter type cannot be a literal type or generic type. Consider using a mapped object type instead. 
+// Понимаю на что жалуется, но не пойму какое решение он предлагает
