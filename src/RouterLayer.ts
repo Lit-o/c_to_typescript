f1dd06f2922fc4743ccf3fd95de46eaf58b63e9a
LitvinenkoBD <boris12lit@gmail.com>
2022-06-16 17:08:32 +0300

    try to typesed index.js again

---
+++ src/RouterLayer.ts
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter, HashRouter, RouteComponentProps } from 'react-router-dom';
+import App from './App';
+import store from './redux/store';
+
+const RouterLayer: React.FC<RouteComponentProps> = () => {
+    return <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+};
+
+export default RouterLayer;
