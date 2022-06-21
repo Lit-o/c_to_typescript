a4923d76966b09af8ad6d44d03ff30c32a7b5745
LitvinenkoBD <boris12lit@gmail.com>
2022-06-21 12:45:11 +0300

    added error to comment in RouterLayers.ts

--- src/RouterLayer.ts
+++ src/RouterLayer.ts
@@ -4,12 +4,14 @@ import { BrowserRouter, HashRouter, RouteComponentProps } from 'react-router-dom
 import App from './App';
 import store from './redux/store';
 
-const RouterLayer: React.FC<RouteComponentProps> = () => {
-    return <BrowserRouter>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </BrowserRouter>
-};
+// const RouterLayer: React.FC<RouteComponentProps<any>> = () => {
+//     return (
+//             <BrowserRouter>
+//                 <Provider store={store} >
+//                     <App/>
+//                 </Provider>
+//             </BrowserRouter>
+//         )
+// };
 
-export default RouterLayer;
+// export default RouterLayer;
